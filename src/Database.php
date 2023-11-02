8fd35263989e7ae6cd11f15450b7633048387738
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-02 10:53:38 +0530

    fatabase connection fixed

--- src/Database.php
+++ src/Database.php
@@ -1,31 +1,34 @@
 <?php
+
 namespace Src;
 
 class Database {
+    private $host;
+    private $port;
+    private $db;
+    private $user;
+    private $pass;
+    
+    private $dbConnection = null;
 
-  private $dbConnection = null;
+    public function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->port = $_ENV['DB_PORT']; // Assuming you have a 'DB_PORT' environment variable
+        $this->db   = $_ENV['DB_DATABASE'];
+        $this->user = $_ENV['DB_USERNAME'];
+        $this->pass = $_ENV['DB_PASSWORD'];
 
-  public function __construct()
-  {
-    $host = $_ENV['DB_HOST'];
-    $port = $_ENV['DB_PORT'];
-    $db   = $_ENV['DB_DATABASE'];
-    $user = $_ENV['DB_USERNAME'];
-    $pass = $_ENV['DB_PASSWORD'];
-
-    try {
-      $this->dbConnection = new \PDO(
-          "mysql:host=$host;port=$port;dbname=$db",
-          $user,
-          $pass
-      );
-    } catch (\PDOException $e) {
-      exit($e->getMessage());
+        try {
+            $this->dbConnection = new \mysqli($this->host, $this->user, $this->pass, $this->db);
+            if ($this->dbConnection->connect_error) {
+                die("Connection failed: " . $this->dbConnection->connect_error);
+            }
+        } catch (\Exception $e) {
+            exit($e->getMessage());
+        }
     }
-  }
 
-  public function connet()
-  {
-    return $this->dbConnection;
-  }
+    public function connect() {
+        return $this->dbConnection;
+    }
 }
