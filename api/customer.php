77329858e846946ec8b1bb000d7fe209eb7e8188
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-03 12:34:15 +0530

    path cheacking

--- api/customer.php
+++ api/customer.php
@@ -10,7 +10,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
-// print_r($uri);
+print_r($uri);
 // customer
 
 // $data = json_decode(file_get_contents("php://input"), true);
