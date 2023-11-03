a2633bf13e11196dbbcde9ba7e717ae065ba78d3
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-03 15:38:35 +0530

    POST and RAW JSON data cheaking function fixed

--- src/utility.php
+++ src/utility.php
@@ -5,20 +5,20 @@ require_once "encryption.php";
 
 function checkPOSTData($name){
     if (isset($_POST[$name])) {
-        echo $_POST[$name];
+        // echo $_POST[$name];
         return $_POST[$name];
     }else {
-        echo 'Not Set';
+        // echo 'Not Set';
         return '';
     }
 }
 
 function checkRAWData($rawInput, $name){
     if (isset($rawInput[$name])) {
-        echo $rawInput[$name];
+        // echo $rawInput[$name];
         return $rawInput[$name];
     }else {
-        echo 'Not Set';
+        // echo 'Not Set';
         return '';
     }
 }
