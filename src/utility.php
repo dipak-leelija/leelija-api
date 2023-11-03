99e94727e5fd3433c5b6f7eb6114943d73a61c0d
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-03 15:30:07 +0530

    checkPOSTData function cheacking

--- src/utility.php
+++ src/utility.php
@@ -5,8 +5,10 @@ require_once "encryption.php";
 
 function checkPOSTData($name){
     if (isset($_POST[$name])) {
+        echo $_POST[$name];
         return $_POST[$name];
     }else {
+        echo 'Not Set';
         return '';
     }
 }
